2be77da3c4fad15d3d3a2c215fae304704c0eba9
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-11-20 14:41:39 +0900

    student model

--- backend/models/student.js
+++ backend/models/student.js
@@ -3,7 +3,17 @@ const { sequelize } = require("../db/connect");
 
 const Student = sequelize.define(
   "Student",
-  {},
+
+  {
+    fullName: {
+      allowNull: false,
+      validate: {
+        notEmpty: {
+          msg: `Please enter a student's name`,
+        },
+      },
+    },
+  },
   { modelName: "Student", tableName: "Student" }
 );
 const syncTable = async () => {
